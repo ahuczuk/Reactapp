385d905ff9762166f02e5ed9c30ceac257344e1b
Agata Huczuk <agatahuczuk@cosibella.pl>
2023-03-16 20:56:56 +0100

    add 11.4

---
+++ src/components/List/List.js
@@ -0,0 +1,13 @@
+import styles from './List.module.scss';
+import Column from '../Column/Column';
+
+const List = () => {
+
+    const columns = [
+        { id: 1, title: 'Books', icon: 'book' },
+      { id: 2, title: 'Movies', icon: 'film' },
+      { id: 3, title: 'Games', icon: 'gamepad' }
+    ];
+}
+
+export default List;
