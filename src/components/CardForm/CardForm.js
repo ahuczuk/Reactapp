1df1065c2a012de7f08688c83398225a957679f1
Agata Huczuk <agatahuczuk@cosibella.pl>
2023-03-17 18:22:00 +0100

    add 11

---
+++ src/components/CardForm/CardForm.js
@@ -0,0 +1,25 @@
+import styles from './CardForm.module.scss';
+import { useState } from 'react';
+import Button from './../Button/Button';
+import TextInput from './../TextInput/TextInput';
+
+const CardForm = props => {
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title: title}, props.columnId);
+        setTitle('');
+    }
+
+
+	return (
+        <form onSubmit={handleSubmit} className={styles.card}>
+        <TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+        <Button>Add Column</Button>
+    </form>
+);
+
+};
+
+export default CardForm;
